dece08f3946c8892341c7bd4846f5f107e873591
sangkyeonglee <sangkyeong.lee93@gmail.com>
2018-05-16 17:47:28 +0900

    .

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/classroom/ClassroomAddServlet.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/classroom/ClassroomAddServlet.java
@@ -47,7 +47,7 @@ public class ClassroomAddServlet extends HttpServlet {
         out.println("<html>");
         out.println("<head>");
         out.println("<meta charset='UTF-8'>");
-//        out.println("<meta http-equiv='Refresh' content='1;url=list'>");
+        out.println("<meta http-equiv='Refresh' content='1;url=list'>");
         out.println("<title>강의실 등록</title>");
         out.println("</head>");
         out.println("<body>");
