4721773c20c673bb76de346ec3b2baf99b49a637
donghyounkang <donghyounkang@gmail.com>
2018-05-16 19:47:15 +0900

    taskdelete 추가

---
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/task/TaskDeleteServlet.java
@@ -0,0 +1,75 @@
+// Controller 규칙에 따라 메서드 작성
+package bitcamp.java106.pms.servlet.task;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bitcamp.java106.pms.dao.TaskDao;
+import bitcamp.java106.pms.dao.TeamDao;
+import bitcamp.java106.pms.server.ServerRequest;
+import bitcamp.java106.pms.server.ServerResponse;
+import bitcamp.java106.pms.servlet.InitServlet;
+
+@SuppressWarnings("serial")
+@WebServlet("/task/delete")
+public class TaskDeleteServlet extends HttpServlet {
+    
+    TeamDao teamDao;
+    TaskDao taskDao;
+    
+    @Override
+    public void init() throws ServletException {
+        teamDao = InitServlet.getApplicationContext().getBean(TeamDao.class);
+        taskDao = InitServlet.getApplicationContext().getBean(TaskDao.class);
+    }
+    
+    @Override
+    protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        int no = Integer.parseInt(request.getParameter("no"));
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset='UTF-8'>");
+        out.println("<meta http-equiv='Refresh' content='1;url=list'>");
+        out.println("<title>작업 삭제</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>작업 삭제 결과</h1>");
+        
+        try {
+            int count = taskDao.delete(no);
+            if (count == 0) {
+                out.println("<p>해당 작업이 존재하지 않습니다.</p>");
+            } else {
+                out.println("<p>삭제하였습니다.</p>");
+            }
+        } catch (Exception e) {
+            out.println("<p>삭제 실패!</p>");
+            out.println("<p>잠시 후 다시 시도해주세요. 계속 오류 발생 시</p> ");
+            out.println("<p>담당자(내선: 120)에게 연락주세요.</p>");
+            e.printStackTrace(out);
+        }
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
+
+//ver 31 - JDBC API가 적용된 DAO 사용
+//ver 28 - 네트워크 버전으로 변경
+//ver 26 - TaskController에서 delete() 메서드를 추출하여 클래스로 정의.
+//ver 23 - @Component 애노테이션을 붙인다.
+//ver 22 - TaskDao 변경 사항에 맞춰 이 클래스를 변경한다.
+//ver 18 - ArrayList가 적용된 TaskDao를 사용한다.
+//ver 17 - 클래스 생성
