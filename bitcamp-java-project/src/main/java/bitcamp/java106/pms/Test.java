5a0625160271a54c3e3e5e0661cb40d7792c0b4a
sangkyeonglee <sangkyeong.lee93@gmail.com>
2018-05-16 14:40:54 +0900

    gg

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/Test.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/Test.java
@@ -11,10 +11,13 @@ public class Test {
         
         ServerRequest request = new ServerRequest(str);
         
+        System.out.println("Hello!! 5팀 화이팅!!");
         System.out.println(request.getServerPath());
         System.out.println(request.getParameter("title"));
         System.out.println(request.getParameter("content"));
         System.out.println(request.getParameter("age"));
+        System.out.println("Bye!!");
+        
         
 
     }
