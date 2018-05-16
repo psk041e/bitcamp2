49afa8455607c9688fe4e14be5ec9f74afc1a6f8
berkleylim <berkleylim16@gmail.com>
2018-05-16 15:56:53 +0900

    test

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/member/MemberDeleteServlet.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/member/MemberDeleteServlet.java
@@ -3,7 +3,6 @@ package bitcamp.java106.pms.servlet.member;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,67 +11,60 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import bitcamp.java106.pms.dao.MemberDao;
-import bitcamp.java106.pms.domain.Board;
-import bitcamp.java106.pms.domain.Member;
-import bitcamp.java106.pms.server.ServerRequest;
-import bitcamp.java106.pms.server.ServerResponse;
 import bitcamp.java106.pms.servlet.InitServlet;
 
 @SuppressWarnings("serial")
-@WebServlet("/member/list")
-public class MemberListServlet extends HttpServlet {
+@WebServlet("/member/delete")
+public class MemberDeleteServlet extends HttpServlet {
 
-    MemberDao memberDao; 
-    
+    MemberDao memberDao;
+
     @Override
     public void init() throws ServletException {
         memberDao = InitServlet.getApplicationContext().getBean(MemberDao.class);
     }
     
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/plain; charset=utf-8");
-        PrintWriter out = response.getWriter();
+    protected void doGet(
+            HttpServletRequest request, 
+            HttpServletResponse response) throws ServletException, IOException {
 
+        String id = request.getParameter("id");
+        
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
         out.println("<!DOCTYPE html>");
         out.println("<html>");
         out.println("<head>");
         out.println("<meta charset='UTF-8'>");
-        out.println("<title>회원 목록</title>");
+        out.println("<meta http-equiv='Refresh' content='1;url=list'>");
+        out.println("<title>회원 삭제</title>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<h1>회원 목록</h1>");
+        out.println("<h1>회원 삭제 결과</h1>");
+        
         try {
-            List<Member> list = memberDao.selectList();
-            
-            out.println("<p><a href='form.html'>새 글</a></p>");
-            out.println("<table border='1'>");
-            out.println("<tr>");
-            out.println("    <th>번호</th><th>제목</th><th>등록</th>");
-            out.println("</tr>");
-            for (Member member : list) {
-                out.println("<tr>");
-                out.printf("    <td>%s</td><td><a href='view?id=%s'>%s</a></td><td>%s</td>\n",
-                    member.getId(), 
-                    member.getId(),
-                    member.getEmail(), 
-                    member.getPassword());
-                out.println("</tr>");
+            int count = memberDao.delete(id);
+    
+            if (count == 0) {
+                out.println("<p>해당 아이디의 회원이 없습니다.</p>");
+            } else {
+                out.println("<p>삭제하였습니다.</p>");
             }
-            out.println("</table>");
         } catch (Exception e) {
-            out.println("<p>목록 가져오기 실패!</p>");
+            out.println("<p>삭제 실패!</p>");
             e.printStackTrace(out);
         }
         out.println("</body>");
         out.println("</html>");
     }
+    
 }
 
 //ver 31 - JDBC API가 적용된 DAO 사용
 //ver 28 - 네트워크 버전으로 변경
-//ver 26 - MemberController에서 list() 메서드를 추출하여 클래스로 정의.
+//ver 26 - MemberController에서 delete() 메서드를 추출하여 클래스로 정의.
 //ver 23 - @Component 애노테이션을 붙인다.
 //ver 22 - MemberDao 변경 사항에 맞춰 이 클래스를 변경한다.
 //ver 18 - ArrayList가 적용된 MemberDao를 사용한다.
