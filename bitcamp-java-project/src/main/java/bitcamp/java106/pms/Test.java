96c32e4a2d40c28a423ba478877c8dba4247db7f
donghyounkang <donghyounkang@gmail.com>
2018-05-16 15:31:07 +0900

    d

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/Test.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/Test.java
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-package bitcamp.java106.pms;
-import bitcamp.java106.pms.server.ServerRequest;
-
-public class Test {
-    
-    public static void main(String[] args) {
-        // 프로그램을 짜다 보면 특정 API를 사용할 때가 있다.
-        // 그 API를 적용하기 전에 간단한 예제를 만들어 동작을 확인하라!
-        String str = "/board/add?title=aaaa&content=bbb";
-        
-        ServerRequest request = new ServerRequest(str);
-        
-        System.out.println("aaaaaseogyeong");
-        System.out.println(request.getServerPath());
-        System.out.println(request.getParameter("title"));
-        System.out.println(request.getParameter("content"));
-        System.out.println(request.getParameter("age"));
-
-        System.out.println("SangKyeongLee");
-        System.out.println("SangKyeongLee");
-        System.out.println("충돌");
-    }
-
-}
-=======
 package bitcamp.java106.pms;
 import bitcamp.java106.pms.server.ServerRequest;
 
@@ -57,4 +31,3 @@ public class Test {
     }
 
 }
->>>>>>> branch 'master' of https://github.com/DonghyounKang/bitcamp2.git
