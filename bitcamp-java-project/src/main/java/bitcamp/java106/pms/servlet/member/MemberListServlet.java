70e92246bd2e3534940b1883acbedaea1426a5af
berkleylim <berkleylim16@gmail.com>
2018-05-16 15:59:48 +0900

    .

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/member/MemberListServlet.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/member/MemberListServlet.java
@@ -49,7 +49,7 @@ public class MemberListServlet extends HttpServlet {
             out.println("<p><a href='form.html'>새 글</a></p>");
             out.println("<table border='1'>");
             out.println("<tr>");
-            out.println("    <th>번호</th><th>제목</th><th>등록</th>");
+            out.println("    <th>회원명</th><th>이메일</th><th>비밀번호</th>");
             out.println("</tr>");
             for (Member member : list) {
                 out.println("<tr>");
